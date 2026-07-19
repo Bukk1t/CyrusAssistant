88ea0f60c2fd8ba486b64c9d6285baa31bbe878b
Bukkit <shantiya1212@gmail.com>
2026-07-19 16:54:55 +0330

    functional commands

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
 from handlers.start import router as start_router
+from handlers.help import router as help_router
+from handlers.about import router as about_router
 
 
 logging.basicConfig(level=logging.INFO)
@@ -15,7 +17,8 @@ dp = Dispatcher()
 
 
 dp.include_router(start_router)
-
+dp.include_router(help_router)
+dp.include_router(about_router)
 
 async def main():
     await dp.start_polling(bot)
