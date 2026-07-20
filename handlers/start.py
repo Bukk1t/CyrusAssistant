80f8aa68600e6817a241dadd242e8089602bdb16
Bukkit <shantiya1212@gmail.com>
2026-07-20 16:05:48 +0330

    Connect start command to user database

--- handlers/start.py
+++ handlers/start.py
@@ -3,6 +3,7 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 
 from keyboards.main import main_keyboard
+from database.database import add_user
 
 
 router = Router()
@@ -10,6 +11,14 @@ router = Router()
 
 @router.message(CommandStart())
 async def start(message: Message):
+    user = message.from_user
+
+    add_user(
+        user_id=user.id,
+        username=user.username,
+        first_name=user.first_name,
+    )
+
     await message.answer(
         "👋 Welcome to Ask Cyrus!\n\n"
         "The bot is online!",
